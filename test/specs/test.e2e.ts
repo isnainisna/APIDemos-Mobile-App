2b1134ce2514fb8b78a3a56d835ac2c60056ad5c
Isnaini Isna <38639096+isnainisna@users.noreply.github.com>
2026-01-10 23:41:21 +0700

    Enhance Text Entry dialog test with screenshots and assertions
    Added screenshot functionality and improved assertions for password visibility in the Text Entry dialog test.

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai"; // Pastikan install chai untuk assertion
 import { APIDemosActions } from "../action/appDemos.action.ts";
 import * as fs from 'fs';
+import { takeScreenshot } from "../takeScreenShoot/takeScreen.ts";
 
 const apiDemosAction = new APIDemosActions();
 let nameInput: string;
@@ -41,19 +42,35 @@ describe("ApiDemos - Text Entry dialog", async () => {
         await driver.terminateApp('io.appium.android.apis');
     })
 
-    it("Verify input name dan password di Text Entry Dialog", async () => {
+    it("Verify input name dan password di Text Entry dialog", async () => {
         // 1. Navigasi ke menu
         await apiDemosAction.navigateToTextEntryDialog();
-
+        await takeScreenshot("1_Buka_Dialog");
         // 2. Isi field name dan password
         await apiDemosAction.inputUserCredentials(nameInput, passInput);
+        await takeScreenshot("2_Isi_Field_Name_Password");
 
         // 3. Ambil nilai dari field untuk verifikasi
         const actualName = await apiDemosAction.getNameValue();
-        const actualPass = await apiDemosAction.getPasswordValue();
+        const isPassVisible = await apiDemosAction.isPasswordDisplayed();
+        const actualPassText = await apiDemosAction.getPasswordValue(); // Mengambil titik-titik
 
-        // 4. Assertion (Verify isi field sesuai input)
+        // 4. Assertion Nama (Teks biasa)
         expect(actualName).to.equal(nameInput, "Nama yang muncul tidak sesuai!");
-        expect(actualPass).to.equal(passInput, "Password yang muncul tidak sesuai!");
-    });
+
+        // 5. Assertion Password
+        // Cek apakah elemennya tampil
+        expect(isPassVisible).to.be.true; 
+        // Cek apakah jumlah titik-titik sesuai dengan panjang input
+        expect(actualPassText.length).to.equal(passInput.length, "Jumlah masking password tidak sesuai!");
+
+        // 6. Klik tombol OK (Langkah tambahan)
+        await apiDemosAction.clickOkButton();
+        await takeScreenshot("3_Klik_Button_OK");
+
+        // 7. Verifikasi dialog tertutup (Opsional tapi bagus untuk automation)
+        // Jika dialog tertutup, elemen inputName seharusnya sudah tidak ada
+        const isDialogClosed = await apiDemosAction.isNameFieldExisting();
+        expect(isDialogClosed).to.be.false;
+    })
 });
